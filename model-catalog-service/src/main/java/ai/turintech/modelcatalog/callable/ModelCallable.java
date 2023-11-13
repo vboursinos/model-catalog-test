29543320628d3ad3c4119fc282e8bd68c84b6357
v.boursinos <v.boursinos@turintech.ai>
2023-11-13 16:34:18 +0000

    chore: remove unused functions from ModelCallable

--- model-catalog-service/src/main/java/ai/turintech/modelcatalog/callable/ModelCallable.java
+++ model-catalog-service/src/main/java/ai/turintech/modelcatalog/callable/ModelCallable.java
@@ -22,30 +22,14 @@ import java.util.stream.Collectors;
 @Scope("prototype")
 public class ModelCallable<T> implements Callable<T> {
     private String name;
-    private UUID id;
     private ModelDTO modelDTO;
 
     private Pageable pageable;
-
-    public ModelCallable(String name) {
-        this.name = name;
-    }
-
-    public ModelCallable(String name, UUID id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public ModelCallable(String name, ModelDTO modelDTO) {
-        this.name = name;
-        this.modelDTO = modelDTO;
-    }
 
     public ModelCallable(String name, Pageable pageable) {
         this.name = name;
         this.pageable = pageable;
     }
-
     @Autowired
     private ModelRepository modelRepository;
 
@@ -72,57 +56,10 @@ public class ModelCallable<T> implements Callable<T> {
         return paginatedList;
     }
 
-    public ModelDTO findById() throws Exception {
-        Optional<Model> model = modelRepository.findOneWithEagerRelationships(id);
-        return model.map(modelMapper::toDto).orElse(null);
-    }
-
-    public ModelDTO create() throws Exception {
-        Model model = modelMapper.toEntity(modelDTO);
-        model = modelRepository.save(model);
-        System.out.println(model.toString());
-        return modelMapper.toDto(model);
-    }
-
-    public ModelDTO update() throws Exception {
-        Model model = modelMapper.toEntity(modelDTO);
-        model = modelRepository.save(model);
-        return modelMapper.toDto(model);
-    }
-
-    public ModelDTO partialUpdate() throws Exception {
-        Optional<Model> result = modelRepository.findById(modelDTO.getId())
-                .map(existingModel -> {
-                    modelMapper.partialUpdate(existingModel, modelDTO);
-                    return existingModel;
-                })
-                .map(modelRepository::save);
-
-        if (result.isPresent()) {
-            return result.map(modelMapper::toDto).get();
-        } else {
-            return null;
-        }
-    }
-
-    public void delete() throws Exception {
-        modelRepository.deleteById(id);
-    }
-
     @Override
     public T call() throws Exception {
-        if (name.equalsIgnoreCase("create")) {
-            return (T) create();
-        } else if (name.equalsIgnoreCase("findAll")) {
+        if (name.equalsIgnoreCase("findAll")) {
             return (T) findAll();
-        } else if (name.equalsIgnoreCase("findById")){
-            return (T) findById();
-        } else if(name.equalsIgnoreCase("update")) {
-            return (T) update();
-        } else if(name.equalsIgnoreCase("partialUpdate")) {
-            return (T) partialUpdate();
-        } else if(name.equalsIgnoreCase("delete")) {
-            delete();
         }
         return null;
     }
