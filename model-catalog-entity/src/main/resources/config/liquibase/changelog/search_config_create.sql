637f3d0bef544c31d05ecd197be58fb7fdcdab6d
v.boursinos <v.boursinos@turintech.ai>
2023-11-20 16:21:56 +0000

    chore: change columns of search_config table for liquibase

--- model-catalog-entity/src/main/resources/config/liquibase/changelog/search_config_create.sql
+++ model-catalog-entity/src/main/resources/config/liquibase/changelog/search_config_create.sql
@@ -1,18 +1,18 @@
 CREATE TABLE SEARCH_CONFIG  (
-	ID  				integer GENERATED ALWAYS AS IDENTITY,
-	HASH_PAQUETE  		varchar(300) NOT NULL,
-	NOMBRE_CLASE  		varchar(300) NOT NULL,
-	NOMBRE_FILTRO 		varchar(50) NOT NULL,
-	NOMBRE_DESTINO 		varchar(50),
-	ACTIVO  			integer default 1,
-	
+	ID  			integer GENERATED ALWAYS AS IDENTITY,
+	PACKAGE_HASH  		varchar(300) NOT NULL,
+	CLASS_NAME  		varchar(300) NOT NULL,
+	FILTER_NAME 		varchar(50) NOT NULL,
+	TARGET_NAME 		varchar(50),
+	ACTIVE 			integer default 1,
+
 	PRIMARY KEY ( ID )
 );
 
-COMMENT ON TABLE SEARCH_CONFIG IS 'Tabla de configuracion del componente igv-search';
-COMMENT ON COLUMN SEARCH_CONFIG.ID IS 'Identificador unico autoincremental';
-COMMENT ON COLUMN SEARCH_CONFIG.HASH_PAQUETE IS 'Hash con el nombre del paquete donde está contenida la clase entidad afectada por la validación (obligatorio)';
-COMMENT ON COLUMN SEARCH_CONFIG.NOMBRE_CLASE IS 'Nombre de la clase entidad afectada por la validación (obligatorio)';
-COMMENT ON COLUMN SEARCH_CONFIG.NOMBRE_FILTRO IS 'Nombre del filtro de entrada (obligatorio)';
-COMMENT ON COLUMN SEARCH_CONFIG.NOMBRE_DESTINO IS 'Nombre con el que se quedará el filtro final de la validación (opcional)';
-COMMENT ON COLUMN SEARCH_CONFIG.ACTIVO IS 'Boolean que indica si el registro está activo';
+COMMENT ON TABLE SEARCH_CONFIG IS 'Configuration table for the component jpa-search';
+COMMENT ON COLUMN SEARCH_CONFIG.ID IS 'Autoincremental identifier';
+COMMENT ON COLUMN SEARCH_CONFIG.PACKAGE_HASH IS 'Package hash name where the entity class is contained (mandatory)';
+COMMENT ON COLUMN SEARCH_CONFIG.CLASS_NAME IS 'Entity class name target of the search (mandatory)';
+COMMENT ON COLUMN SEARCH_CONFIG.FILTER_NAME IS 'Name of the filter name (mandatory)';
+COMMENT ON COLUMN SEARCH_CONFIG.TARGET_NAME IS 'Name of the search filter (optional)';
+COMMENT ON COLUMN SEARCH_CONFIG.ACTIVE IS 'Boolean value that indicates the row being active.';
