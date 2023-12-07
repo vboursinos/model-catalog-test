7931d465d271d289f2b8a77e6c4c7329ba8313c0
v.boursinos <v.boursinos@turintech.ai>
2023-12-07 11:10:02 +0000

    chore: add comments

--- model-catalog-api/src/main/java/ai/turintech/modelcatalog/callable/GenericModelCallable.java
+++ model-catalog-api/src/main/java/ai/turintech/modelcatalog/callable/GenericModelCallable.java
@@ -8,18 +8,50 @@ import java.util.UUID;
 
 public interface GenericModelCallable<T, DTO extends AbstractDTO, ENTITY extends AbstractEntity>
     extends ReactiveAbstractCrudCallable<T, DTO, ENTITY, UUID> {
+
+  /**
+   * Find all entities
+   *
+   * @return list of entities
+   */
   public List<DTO> findAll();
 
+  /**
+   * Find entity by id
+   *
+   * @return entity
+   */
   public DTO findById() throws Exception;
 
+  /**
+   * Check if entity exists by id
+   *
+   * @return true if entity exists
+   */
   public Boolean existsById();
 
+  /**
+   * Create entity
+   *
+   * @return created entity
+   */
   public DTO create();
 
+  /**
+   * Update entity
+   *
+   * @return updated entity
+   */
   public DTO update();
 
+  /**
+   * Partially update entity
+   *
+   * @return partially updated entity
+   */
   public DTO partialUpdate() throws Exception;
 
+  /** Delete entity */
   public void delete();
 
   public T call() throws Exception;
