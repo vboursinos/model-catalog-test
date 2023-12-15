20fa703306298edd7e3a3eb13be37656c5fece81
v.boursinos <v.boursinos@turintech.ai>
2023-12-15 17:06:55 +0000

    chore: remove after mapping

--- model-catalog-dto-entity-mapper/src/main/java/ai/turintech/modelcatalog/dtoentitymapper/BooleanParameterMapper.java
+++ model-catalog-dto-entity-mapper/src/main/java/ai/turintech/modelcatalog/dtoentitymapper/BooleanParameterMapper.java
@@ -3,19 +3,9 @@ package ai.turintech.modelcatalog.dtoentitymapper;
 import ai.turintech.components.mapper.api.MapperInterface;
 import ai.turintech.modelcatalog.dto.BooleanParameterDTO;
 import ai.turintech.modelcatalog.entity.BooleanParameter;
-import ai.turintech.modelcatalog.entity.ParameterTypeDefinition;
 import org.mapstruct.*;
 
 /** Mapper for the entity {@link BooleanParameter} and its DTO {@link BooleanParameterDTO}. */
 @Mapper(componentModel = "spring")
 public interface BooleanParameterMapper
-    extends MapperInterface<BooleanParameterDTO, BooleanParameter> {
-
-  @AfterMapping
-  default void afterMapping(
-      @MappingTarget BooleanParameter booleanParameter, BooleanParameterDTO booleanParameterDTO) {
-    ParameterTypeDefinition parameterTypeDefinition = new ParameterTypeDefinition();
-    parameterTypeDefinition.setId(booleanParameterDTO.getParameterTypeDefinitionId());
-    booleanParameter.setParameterTypeDefinition(parameterTypeDefinition);
-  }
-}
+    extends MapperInterface<BooleanParameterDTO, BooleanParameter> {}
