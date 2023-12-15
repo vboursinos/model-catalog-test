eb844a97a0968713dc30bec22c4e641bcbef764b
v.boursinos <v.boursinos@turintech.ai>
2023-12-15 15:10:06 +0000

    chore: use findAll method from java-components

--- model-catalog-service/src/main/java/ai/turintech/modelcatalog/service/CategoricalParameterServiceImpl.java
+++ model-catalog-service/src/main/java/ai/turintech/modelcatalog/service/CategoricalParameterServiceImpl.java
@@ -2,15 +2,10 @@ package ai.turintech.modelcatalog.service;
 
 import ai.turintech.components.architecture.service.impl.reactive.ReactiveAbstractUUIDIdentityCrudServiceImpl;
 import ai.turintech.modelcatalog.dto.CategoricalParameterDTO;
-import ai.turintech.modelcatalog.dtoentitymapper.CategoricalParameterMapper;
 import ai.turintech.modelcatalog.entity.CategoricalParameter;
-import ai.turintech.modelcatalog.repository.CategoricalParameterRepository;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 /** Service Implementation for managing {@link CategoricalParameter}. */
 @Service
@@ -18,14 +13,4 @@ import reactor.core.scheduler.Schedulers;
 public class CategoricalParameterServiceImpl
     extends ReactiveAbstractUUIDIdentityCrudServiceImpl<
         CategoricalParameterDTO, CategoricalParameter, UUID>
-    implements CategoricalParameterService {
-  @Autowired private CategoricalParameterRepository repository;
-  @Autowired private CategoricalParameterMapper mapperInterface;
-
-  @Override
-  @Transactional
-  public Flux<CategoricalParameterDTO> findAllStream() {
-    return Flux.fromIterable(repository.findAll().stream().map(mapperInterface::to).toList())
-        .subscribeOn(Schedulers.boundedElastic());
-  }
-}
+    implements CategoricalParameterService {}
--- model-catalog-service/src/main/java/ai/turintech/modelcatalog/service/IntegerParameterServiceImpl.java
+++ model-catalog-service/src/main/java/ai/turintech/modelcatalog/service/IntegerParameterServiceImpl.java
@@ -2,30 +2,14 @@ package ai.turintech.modelcatalog.service;
 
 import ai.turintech.components.architecture.service.impl.reactive.ReactiveAbstractUUIDIdentityCrudServiceImpl;
 import ai.turintech.modelcatalog.dto.IntegerParameterDTO;
-import ai.turintech.modelcatalog.dtoentitymapper.IntegerParameterMapper;
 import ai.turintech.modelcatalog.entity.IntegerParameter;
-import ai.turintech.modelcatalog.repository.IntegerParameterRepository;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 /** Service Implementation for managing {@link IntegerParameter}. */
 @Service
 @Transactional
 public class IntegerParameterServiceImpl
     extends ReactiveAbstractUUIDIdentityCrudServiceImpl<IntegerParameterDTO, IntegerParameter, UUID>
-    implements IntegerParameterService {
-
-  @Autowired private IntegerParameterRepository repository;
-  @Autowired private IntegerParameterMapper mapperInterface;
-
-  @Override
-  @Transactional
-  public Flux<IntegerParameterDTO> findAllStream() {
-    return Flux.fromIterable(repository.findAll().stream().map(mapperInterface::to).toList())
-        .subscribeOn(Schedulers.boundedElastic());
-  }
-}
+    implements IntegerParameterService {}
