ae66d73b322dc933d019aae39945fe40a0909027
v.boursinos <v.boursinos@turintech.ai>
2024-01-15 20:43:19 +0000

    chore: add mappers and interfaces for dependency group

---
+++ model-catalog-api/src/main/java/ai/turintech/modelcatalog/facade/DependencyGroupTypeFacade.java
@@ -0,0 +1,7 @@
+package ai.turintech.modelcatalog.facade;
+
+import ai.turintech.components.architecture.reactive.ReactiveUUIDIdentityCrudFacade;
+import ai.turintech.modelcatalog.dto.DependencyGroupTypeDTO;
+
+public interface DependencyGroupTypeFacade
+    extends ReactiveUUIDIdentityCrudFacade<DependencyGroupTypeDTO> {}
