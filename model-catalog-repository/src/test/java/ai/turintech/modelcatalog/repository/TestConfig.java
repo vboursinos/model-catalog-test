39aaf2c175f6201d34034f709f510cde71bc0adf
v.boursinos <v.boursinos@turintech.ai>
2024-01-16 18:00:48 +0000

    chore: tests updated

--- model-catalog-repository/src/test/java/ai/turintech/modelcatalog/repository/TestConfig.java
+++ model-catalog-repository/src/test/java/ai/turintech/modelcatalog/repository/TestConfig.java
@@ -1,8 +1,8 @@
 package ai.turintech.modelcatalog.repository;
 
 import ai.turintech.modelcatalog.entity.ModelCatalogEntityPackage;
-import javax.sql.DataSource;
 import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,37 +22,36 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EntityScan(basePackageClasses = {ModelCatalogEntityPackage.class})
 public class TestConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.postgresql.Driver");
     dataSource.setUrl(MetricRepositoryTest.postgreSQLContainer.getJdbcUrl());
     dataSource.setUsername(MetricRepositoryTest.postgreSQLContainer.getUsername());
     dataSource.setPassword(MetricRepositoryTest.postgreSQLContainer.getPassword());
-        return dataSource;
-    }
+    return dataSource;
+  }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
+  @Bean
+  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("ai.turintech.modelcatalog.entity"); // Package where your JPA entities are located
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource);
+    em.setPackagesToScan(
+        "ai.turintech.modelcatalog.entity"); // Package where your JPA entities are located
     // Additional JPA configuration can be added here
     JpaVendorAdapter vendorAdapter =
         new HibernateJpaVendorAdapter(); // or any other JPA vendor adapter
-        em.setJpaVendorAdapter(vendorAdapter);
-        return em;
-    }
+    em.setJpaVendorAdapter(vendorAdapter);
+    return em;
+  }
 
-    @Bean
-    public PlatformTransactionManager transactionManager(
-            EntityManagerFactory entityManagerFactory) {
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
     return new JpaTransactionManager(entityManagerFactory);
-    }
+  }
 }
--- model-catalog-rest/src/test/java/ai/turintech/modelcatalog/rest/TestConfig.java
+++ model-catalog-rest/src/test/java/ai/turintech/modelcatalog/rest/TestConfig.java
@@ -1,28 +1,26 @@
 package ai.turintech.modelcatalog.rest;
 
-
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class TestConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(MetricControllerTest.postgreSQLContainer.getJdbcUrl());
-        dataSource.setUsername(MetricControllerTest.postgreSQLContainer.getUsername());
-        dataSource.setPassword(MetricControllerTest.postgreSQLContainer.getPassword());
-        return dataSource;
-    }
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.postgresql.Driver");
+    dataSource.setUrl(MetricControllerTest.postgreSQLContainer.getJdbcUrl());
+    dataSource.setUsername(MetricControllerTest.postgreSQLContainer.getUsername());
+    dataSource.setPassword(MetricControllerTest.postgreSQLContainer.getPassword());
+    return dataSource;
+  }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
+  @Bean
+  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
 }
