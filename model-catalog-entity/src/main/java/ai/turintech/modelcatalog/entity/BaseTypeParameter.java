27737ee0e7b41f8af486254e35d7c91d9c66e52d
v.boursinos <v.boursinos@turintech.ai>
2024-01-25 13:51:37 +0000

    chore: remove id property from BaseTypeParameter

--- model-catalog-entity/src/main/java/ai/turintech/modelcatalog/entity/BaseTypeParameter.java
+++ model-catalog-entity/src/main/java/ai/turintech/modelcatalog/entity/BaseTypeParameter.java
@@ -2,27 +2,13 @@ package ai.turintech.modelcatalog.entity;
 
 import ai.turintech.components.data.common.entity.AbstractUUIDIdentityEntity;
 import jakarta.persistence.*;
-import java.io.Serializable;
-import java.util.UUID;
 
 @MappedSuperclass
-public abstract class BaseTypeParameter extends AbstractUUIDIdentityEntity implements Serializable {
-
-  @Id
-  @Column(name = "id", insertable = true, updatable = false)
-  protected UUID id;
+public abstract class BaseTypeParameter extends AbstractUUIDIdentityEntity {
 
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "id", unique = true)
   protected ParameterTypeDefinition parameterTypeDefinition;
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
 
   public ParameterTypeDefinition getParameterTypeDefinition() {
     return parameterTypeDefinition;
