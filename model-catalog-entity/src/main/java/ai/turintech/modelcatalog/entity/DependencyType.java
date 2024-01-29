a9690134f0598de54b3949a80cad2e15de306877
v.boursinos <v.boursinos@turintech.ai>
2024-01-29 16:29:29 +0000

    chore: minor fix

--- model-catalog-entity/src/main/java/ai/turintech/modelcatalog/entity/DependencyType.java
+++ model-catalog-entity/src/main/java/ai/turintech/modelcatalog/entity/DependencyType.java
@@ -66,8 +66,6 @@ public class DependencyType extends AbstractUUIDIdentityEntity {
         + "name='"
         + name
         + '\''
-        + ", dependencyGroupType="
-        + dependencyGroupType
         + '}';
   }
 }
