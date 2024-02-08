1d050fcdbdf21fce1bb6afec1fc59f80866c0e59
v.boursinos <v.boursinos@turintech.ai>
2024-02-08 11:46:25 +0000

    chore: sentry as profile

--- model-catalog-rest/src/main/java/ai/turintech/modelcatalog/rest/reporting/SentryIntegrationWrapperImpl.java
+++ model-catalog-rest/src/main/java/ai/turintech/modelcatalog/rest/reporting/SentryIntegrationWrapperImpl.java
@@ -1,9 +1,11 @@
 package ai.turintech.modelcatalog.rest.reporting;
 
 import io.sentry.Sentry;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /** Bean implementation just to wrap Sentry client invocation. */
+@Profile("sentry")
 @Component
 public class SentryIntegrationWrapperImpl implements SentryIntegrationWrapper {
   public void captureException(Throwable throwable) {
