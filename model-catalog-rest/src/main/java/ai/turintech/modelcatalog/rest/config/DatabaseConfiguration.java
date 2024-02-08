eb9ba43c24b4e1052927651b860ad7428f15f954
v.boursinos <v.boursinos@turintech.ai>
2024-02-08 14:36:24 +0000

    chore: merge databaseConfiguration with JpaConfiguration

--- model-catalog-rest/src/main/java/ai/turintech/modelcatalog/rest/config/DatabaseConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ai.turintech.modelcatalog.rest.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement
-public class DatabaseConfiguration {}
