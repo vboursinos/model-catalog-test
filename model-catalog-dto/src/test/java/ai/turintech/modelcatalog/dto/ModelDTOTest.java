6a0ea43c0f5af75c4a6b84c5b7a41d34e6129e21
v.boursinos <v.boursinos@turintech.ai>
2024-03-25 09:25:28 +0000

    test: remove unused mockito import

--- model-catalog-dto/src/test/java/ai/turintech/modelcatalog/dto/ModelDTOTest.java
+++ model-catalog-dto/src/test/java/ai/turintech/modelcatalog/dto/ModelDTOTest.java
@@ -1,7 +1,6 @@
 package ai.turintech.modelcatalog.dto;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
 import java.util.List;
--- model-catalog-dto/src/test/java/ai/turintech/modelcatalog/dto/ParameterTypeDTOTest.java
+++ model-catalog-dto/src/test/java/ai/turintech/modelcatalog/dto/ParameterTypeDTOTest.java
@@ -1,7 +1,6 @@
 package ai.turintech.modelcatalog.dto;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
