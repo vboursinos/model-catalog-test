510f24ac609ca90a26150219fa7d6894cd3ac4a6
v.boursinos <v.boursinos@turintech.ai>
2024-04-28 19:53:18 +0100

    chore: delete unused migration files

--- model-catalog-migration-file-creator/src/main/resources/liquibase/changelog_post/5-DML-insert_group_type.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO model_group_type(name) VALUES ('fast');
-INSERT INTO revinfo(rev,revtstmp) VALUES ( COALESCE (( select max(rev)+1 from revinfo), 1 ), '2024-04-17 16:31:23');
-INSERT INTO model_group_type_AUD(id, name, rev, revtype, created_at, updated_at) VALUES ((select id from model_group_type where name='fast'), 'fast', (select max(rev) from revinfo),0,'2024-04-17 16:31:23','2024-04-17 16:31:23');
-INSERT INTO model_group_type(name) VALUES ('explainable');
-INSERT INTO revinfo(rev,revtstmp) VALUES ( COALESCE (( select max(rev)+1 from revinfo), 1 ), '2024-04-17 16:31:23');
-INSERT INTO model_group_type_AUD(id, name, rev, revtype, created_at, updated_at) VALUES ((select id from model_group_type where name='explainable'), 'explainable', (select max(rev) from revinfo),0,'2024-04-17 16:31:23','2024-04-17 16:31:23');
-INSERT INTO model_group_type(name) VALUES ('advanced');
-INSERT INTO revinfo(rev,revtstmp) VALUES ( COALESCE (( select max(rev)+1 from revinfo), 1 ), '2024-04-17 16:31:23');
-INSERT INTO model_group_type_AUD(id, name, rev, revtype, created_at, updated_at) VALUES ((select id from model_group_type where name='advanced'), 'advanced', (select max(rev) from revinfo),0,'2024-04-17 16:31:23','2024-04-17 16:31:23');
--- model-catalog-migration-file-creator/src/main/resources/liquibase/changelog_post/6-DML-insert_metrics.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO metric(name) VALUES ('classification-log-loss');
-INSERT INTO revinfo(rev,revtstmp) VALUES ( COALESCE (( select max(rev)+1 from revinfo), 1 ), '2024-04-17 16:31:23');
-INSERT INTO metric_AUD(id, name, rev, revtype, created_at, updated_at) VALUES ((select id from metric where name='classification-log-loss'), 'classification-log-loss', (select max(rev) from revinfo),0,'2024-04-17 16:31:23','2024-04-17 16:31:23');
-INSERT INTO metric(name) VALUES ('classification-roc');
-INSERT INTO revinfo(rev,revtstmp) VALUES ( COALESCE (( select max(rev)+1 from revinfo), 1 ), '2024-04-17 16:31:23');
-INSERT INTO metric_AUD(id, name, rev, revtype, created_at, updated_at) VALUES ((select id from metric where name='classification-roc'), 'classification-roc', (select max(rev) from revinfo),0,'2024-04-17 16:31:23','2024-04-17 16:31:23');
