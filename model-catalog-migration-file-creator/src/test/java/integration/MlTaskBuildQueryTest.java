1da7e57af9c5e1d66c551b2485c206a93de77420
v.boursinos <v.boursinos@turintech.ai>
2024-05-01 21:35:13 +0100

    test: add integration test

---
+++ model-catalog-migration-file-creator/src/test/java/integration/MlTaskBuildQueryTest.java
@@ -0,0 +1,42 @@
+package integration;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import migration_files_creator.static_query_creator.MlTaskCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import unit.static_query_creator.BaseQueryValidationTest;
+
+@SpringBootTest
+@Testcontainers
+@ContextConfiguration(classes = TestConfig.class)
+public class MlTaskBuildQueryTest extends BaseQueryValidationTest {
+
+  @Autowired private MlTaskCreator mlTaskCreator;
+
+  private static final String FILE_NAME = "src/test/java/integration/migration_file.sql";
+
+  @Test
+  public void testParameterTypeQuery() {
+    mlTaskCreator.createStaticTable(FILE_NAME);
+    File file = new File(FILE_NAME);
+    Assertions.assertTrue(file.exists() && file.length() > 0);
+    validateContent();
+    file.delete();
+  }
+
+  private void validateContent() {
+    try (BufferedReader br =
+        new BufferedReader(new FileReader(FILE_NAME, Charset.defaultCharset()))) {
+      String firstLine = br.readLine();
+      Assertions.assertTrue(
+          firstLine.contains("INSERT INTO ml_task_type(name) VALUES ('classification_2');"));
+    } catch (IOException e) {
+      System.err.println("Error reading the file: " + e.getMessage());
+    }
+  }
+}
