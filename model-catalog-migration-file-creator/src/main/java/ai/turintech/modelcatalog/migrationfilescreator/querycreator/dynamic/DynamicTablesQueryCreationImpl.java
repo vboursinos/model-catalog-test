089b0f8fb2c17a876004ec42c29874773ecb8f16
v.boursinos <v.boursinos@turintech.ai>
2024-05-16 09:48:31 +0100

    chore: insert metaml version at migration file name

--- model-catalog-migration-file-creator/src/main/java/ai/turintech/modelcatalog/migrationfilescreator/querycreator/dynamic/DynamicTablesQueryCreationImpl.java
+++ model-catalog-migration-file-creator/src/main/java/ai/turintech/modelcatalog/migrationfilescreator/querycreator/dynamic/DynamicTablesQueryCreationImpl.java
@@ -9,7 +9,10 @@ import ai.turintech.modelcatalog.migrationfilescreator.querycreator.dynamic.quer
 import ai.turintech.modelcatalog.migrationfilescreator.utils.FileUtils;
 import ai.turintech.modelcatalog.service.ModelService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,6 +29,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class DynamicTablesQueryCreationImpl implements DynamicTablesQueryCreation {
   private static final Logger logger = LogManager.getLogger(InsertDynamicTablesImpl.class);
   private static final String JSON_DIR_PATH = "model-catalog-migration-file-creator/model_infos";
+  private static final String PYTHON_REQ_PATH =
+      "model-catalog-migration-file-creator/model-catalog-py/setup/requirements.txt";
 
   @Autowired private ModelService modelService;
   @Autowired private InsertDynamicTables insertDynamicTables;
@@ -50,7 +55,8 @@ public class DynamicTablesQueryCreationImpl implements DynamicTablesQueryCreatio
     Path dirPath = Paths.get(JSON_DIR_PATH);
     count = FileUtils.countFiles(liquibaseDirPath) + 1;
     outputFileName =
-        Paths.get(outputFilePath, count + defaultLiquibaseFileName + ".sql").toString();
+        Paths.get(outputFilePath, count + defaultLiquibaseFileName + getMetaMlVersion() + ".sql")
+            .toString();
     Path outputFile = Paths.get(outputFileName);
     if (!Files.exists(outputFile)) {
       createConstantSqlFile(constantSQL);
@@ -96,5 +102,21 @@ public class DynamicTablesQueryCreationImpl implements DynamicTablesQueryCreatio
 
   private String cleanupSQLScript(String sqlScript) {
     return sqlScript.replaceAll("(?m)^[ \t]*\r?\n", "");
+  }
+
+  private String getMetaMlVersion() {
+    String metamlPrefix = "metaml==";
+    try (BufferedReader reader =
+        new BufferedReader(new FileReader(PYTHON_REQ_PATH, Charset.defaultCharset()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (line.startsWith(metamlPrefix)) {
+          return line.substring(metamlPrefix.length());
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
   }
 }
